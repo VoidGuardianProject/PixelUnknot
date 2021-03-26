a01357fbaf2edbfc7ba27fefa1c3d1b281e8e7da
VoidGuardianProject <TheVoidGuardiand@protonmail.com>
2021-03-26 17:05:37 -0400

    Updated detect.py to Python3 from Python2

--- gather.sh
+++ gather.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 if [ -z "$3" ]; then
-  usage "$0 domain board threadid" 
+  usage "$0 domain board threadid"
   exit
 fi
 
-#  which chan and board
+# Which chan forum / board to scrape
 DOMAIN=$1
 BOARD=$2
 THREAD=$3
 
+# URL variable
 URL=https://boards.$DOMAIN/$BOARD/thread/$THREAD
+# Images
 IMGS=is2.$DOMAIN
 
+# Get those images
 wget -P $THREAD -nd -np -r -l 1 -e robots=off -H -D $IMGS -A jpg,jpeg $URL
-for F in $THREAD/*.jp*g; do python detect.py $F; done
+
+# loop over them and run the python detect.py file
+for F in $THREAD/*.jp*g;
+    do python detect.py $F;
+done
